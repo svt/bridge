dc5776b308c7004ed17a95ac393ddba46ec160ad
Axel Boberg <axel.boberg@svt.se>
2022-01-19 12:15:55 +0100

    Fix an issue trying to bundle the readme as a plugin

--- plugins/webpack.config.js
+++ plugins/webpack.config.js
@@ -2,12 +2,22 @@ const fs = require('fs')
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+const PLUGINS_DIR = './plugins'
+
 /**
  * Webpack configurations
  * for each of the plugins
  */
-const plugins = fs.readdirSync('./plugins')
-  .filter(pathname => !pathname.includes('.js'))
+const plugins = fs.readdirSync(PLUGINS_DIR)
+  .filter(pathname => {
+    /*
+    Filter out paths that are not directories,
+    since we keep a few other files in the plugins
+    directory as well
+    */
+    const pluginPath = path.join(PLUGINS_DIR, pathname)
+    return fs.statSync(pluginPath).isDirectory()
+  })
   .map(plugin => {
     /**
      * Require the plugin's manifest in order to
