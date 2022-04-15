9fbf3f15ba60665cc0e14cdbf1b5b3c195a4c23d
Axel Boberg <git@axelboberg.se>
2022-04-15 17:21:32 +0200

    Add api functions for the rundown
    Signed-off-by: Axel Boberg <git@axelboberg.se>

--- plugins/rundown/index.js
+++ plugins/rundown/index.js
@@ -10,6 +10,8 @@ const bridge = require('bridge')
 const assets = require('../../assets.json')
 const manifest = require('./package.json')
 
+const PLUGIN_STATE_SCOPE = 'bridge-plugin-rundown'
+
 async function initWidget () {
   const cssPath = `${assets.hash}.${manifest.name}.bundle.css`
   const jsPath = `${assets.hash}.${manifest.name}.bundle.js`
@@ -36,4 +38,85 @@ async function initWidget () {
 
 exports.activate = async () => {
   initWidget()
+
+  /**
+   * Get the items of a rundown
+   * by the rundown's id
+   * @param { String } rundownId The id of the rundown to get items for
+   * @returns { String[] }
+   */
+  async function getItems (rundownId) {
+    const state = await bridge.state.get()
+    return state?.plugins?.[PLUGIN_STATE_SCOPE]?.rundowns?.[rundownId]?.items
+  }
+
+  bridge.commands.registerCommand('rundown.reorderItem', async (rundownId, itemId, newIndex) => {
+    const items = await getItems(rundownId)
+    const index = items.indexOf(itemId)
+
+    if (index === -1) return
+
+    /*
+    Remove the id from
+    the array of items
+    */
+    items.splice(index, 1)
+
+    /*
+    Insert the id at
+    the desired index
+    */
+    items.splice(newIndex, 0, itemId)
+
+    bridge.state.apply({
+      plugins: {
+        [PLUGIN_STATE_SCOPE]: {
+          rundowns: {
+            [rundownId]: {
+              items: { $replace: items }
+            }
+          }
+        }
+      }
+    })
+  })
+
+  bridge.commands.registerCommand('rundown.removeItem', async (rundownId, itemId) => {
+    const items = await getItems(rundownId)
+    const index = items.indexOf(itemId)
+
+    if (index === -1) return
+
+    /*
+    Remove the id from
+    the array of items
+    */
+    items.splice(index, 1)
+
+    bridge.state.apply({
+      plugins: {
+        [PLUGIN_STATE_SCOPE]: {
+          rundowns: {
+            [rundownId]: {
+              items: { $replace: items }
+            }
+          }
+        }
+      }
+    })
+  })
+
+  bridge.commands.registerCommand('rundown.appendItem', (rundownId, itemId) => {
+    bridge.state.apply({
+      plugins: {
+        [PLUGIN_STATE_SCOPE]: {
+          rundowns: {
+            [rundownId]: {
+              items: [itemId]
+            }
+          }
+        }
+      }
+    })
+  })
 }
