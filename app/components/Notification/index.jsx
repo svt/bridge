34d76719a77a7fbf5698af350db4e0ef06e90146
Axel Boberg <git@axelboberg.se>
2023-04-12 22:21:59 +0200

    Add ability to add and remove items in the grid
    Signed-off-by: Axel Boberg <git@axelboberg.se>

--- app/components/Notification/index.jsx
+++ app/components/Notification/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.css'
 
-export function Notification ({ type, size = 'normal', content, disableInteraction }) {
+export function Notification ({ type, size = 'normal', title, description, interactionEnabled }) {
   const [isHidden, setIsHidden] = React.useState(false)
 
   function handleHideBtnClick () {
@@ -18,9 +18,16 @@ export function Notification ({ type, size = 'normal', content, disableInteracti
 
   return (
     <div className={`Notification Notification--${type} Notification-size--${size}`}>
-      <div className='Notification-content' dangerouslySetInnerHTML={{ __html: content }} />
+      <div className='Notification-content'>
+        {
+          title && <span className='Notification-contentSection Notification-title'>{title}</span>
+        }
+        {
+          description && <span className='Notification-contentSection Notification-description'>{description}</span>
+        }
+      </div>
       {
-        !disableInteraction
+        interactionEnabled
           ? (
             <div className='Notification-controls'>
               <button className='Notification-hideBtn Link' onClick={handleHideBtnClick}>Dölj</button>
