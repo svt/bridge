502e3e84608bd035c19f6da043e230c26ba2c674
Axel Boberg <git@axelboberg.se>
2023-05-04 22:47:54 +0200

    Create items by dragging from the library
    Signed-off-by: Axel Boberg <git@axelboberg.se>

--- plugins/caspar/app/components/LibraryListItem/index.jsx
+++ plugins/caspar/app/components/LibraryListItem/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './style.css'
 
+import bridge from 'bridge'
+
 export const LibraryListItem = ({ item = {} }) => {
+  async function handleDragStart (e) {
+    e.dataTransfer.setData('itemSpec', JSON.stringify({
+      type: 'bridge.caspar.amcp',
+      data: {
+        name: item.name
+      }
+    }))
+    e.stopPropagation()
+  }
+
+  function handleDragEnd (e) {
+    console.log(e)
+  }
+
   return (
-    <li className='LibraryListItem'>
+    <li className='LibraryListItem' onDragStart={e => handleDragStart(e)} onDragEnd={e => handleDragEnd(e)} draggable>
       <div className='LibraryListItem-name LibraryListItem-col'>
         {item?.name}
       </div>
