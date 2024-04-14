b606802eae1c2701d14d620aef672c00cc778a23
Axel Boberg <git@axelboberg.se>
2024-04-15 00:41:34 +0200

    Add warnings when a numeric value gets out of bounds
    Signed-off-by: Axel Boberg <git@axelboberg.se>

--- app/components/PreferencesNumberInput/index.jsx
+++ app/components/PreferencesNumberInput/index.jsx
@@ -5,10 +5,33 @@ import * as random from '../../utils/random'
 
 export function PreferencesNumberInput ({ label, value = '', min = 0, max = 10, onChange = () => {} }) {
   const [id] = React.useState(`number-${random.number()}`)
+  const [error, setError] = React.useState()
+
+  function handleChange (newValue) {
+    if (newValue < min) {
+      setError(`Cannot be less than ${min}`)
+      onChange(newValue)
+      return
+    }
+
+    if (newValue > max) {
+      setError(`Cannot be more than ${max}`)
+      onChange(newValue)
+      return
+    }
+
+    setError(undefined)
+    onChange(newValue)
+  }
+
   return (
     <div className='PreferencesNumberInput'>
-      <input id={id} className='PreferencesNumberInput-input' type='number' min={min} max={max} value={value} onChange={e => onChange(e.target.value)} />
+      <input id={id} className='PreferencesNumberInput-input' type='number' min={min} max={max} value={value} onChange={e => handleChange(e.target.value)} />
       <label htmlFor={id}>{label}</label>
+      {
+        error &&
+        <div className='PreferencesNumberInput-error'>{error}</div>
+      }
     </div>
   )
 }
