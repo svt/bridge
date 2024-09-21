53b67db205cb8916a08ff16d77a78aa8b2fc2370
Axel Boberg <git@axelboberg.se>
2024-09-21 22:56:24 +0200

    Add renderValue to the documentation
    Signed-off-by: Axel Boberg <git@axelboberg.se>

--- api/items.js
+++ api/items.js
@@ -360,6 +360,26 @@ async function removeIssue (itemId, issueId) {
 }
 exports.removeIssue = removeIssue
 
+/**
+ * Render a value for an item by its id,
+ * this will replace any variable placeholders
+ *
+ * @example
+ *
+ * Item {
+ *  id: '1234',
+ *  data: {
+ *    name: '$(this.data.myValue)',
+ *    myValue: 'Hello World'
+ *  }
+ * }
+ *
+ * renderValue('1234', 'data.name') -> 'Hello World'
+ *
+ * @param { String } itemId The id of the item
+ * @param { String } path The path to the value to render
+ * @returns { Promise.<String | any | undefined> }
+ */
 async function renderValue (itemId, path) {
   const item = await getItem(itemId)
   const currentValue = objectPath.get(item || {}, path)
