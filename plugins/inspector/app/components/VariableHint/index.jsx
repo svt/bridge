426fbb52e35eea23d09bb43ae55fe316fceddc50
Axel Boberg <git@axelboberg.se>
2024-12-31 02:13:27 +0100

    Provide auto completion for variables in the inspector
    Signed-off-by: Axel Boberg <git@axelboberg.se>

---
+++ plugins/inspector/app/components/VariableHint/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style.css'
+
+export function VariableHint ({}) {
+  return (
+    <div className='VariableHint' data-hint='Supports variables as $(name)' data-hint-alignment='right'>VAR</div>
+  )
+}
