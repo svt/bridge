736b382676ccd4c55e1fa6f1405f273d62efbc17
Axel Boberg <git@axelboberg.se>
2025-02-23 10:33:14 +0100

    Add onEnd actions
    Signed-off-by: Axel Boberg <git@axelboberg.se>

--- plugins/rundown/app/components/RundownItemIndicatorsSection/index.jsx
+++ plugins/rundown/app/components/RundownItemIndicatorsSection/index.jsx
@@ -4,7 +4,7 @@ import { Icon } from '../Icon'
 
 import './style.css'
 
-const ON_PLAY_ENUM = {
+const ACTIONS_ENUM = {
   'SELECT_NEXT_ITEM': '2',
   'PLAY_NEXT_ITEM': '1' 
 }
@@ -13,11 +13,18 @@ export function RundownItemIndicatorsSection ({ item }) {
   return (
     <div className='RundownItemIndicatorsSection'>
       {
-        item?.data?.onPlay === ON_PLAY_ENUM.SELECT_NEXT_ITEM &&
+        (
+          item?.data?.onPlay === ACTIONS_ENUM.SELECT_NEXT_ITEM ||
+          item?.data?.onEnd === ACTIONS_ENUM.SELECT_NEXT_ITEM
+        )
+        &&
           <span className='RundownItemIndicatorsSection-icon'><Icon name='arrowDownSecondary' /></span>
       }
       {
-        item?.data?.onPlay === ON_PLAY_ENUM.PLAY_NEXT_ITEM &&
+        (
+          item?.data?.onPlay === ACTIONS_ENUM.PLAY_NEXT_ITEM ||
+          item?.data?.onEnd === ACTIONS_ENUM.PLAY_NEXT_ITEM
+        ) &&
           <span className='RundownItemIndicatorsSection-icon'><Icon name='arrowDownPlay' /></span>
       }
       {
