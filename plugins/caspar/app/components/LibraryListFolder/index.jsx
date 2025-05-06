6e9e2f604080adc5eb831d687beb5a262fa271d6
Simon Mellberg <mellbergsimon@example.com>
2025-05-06 09:58:26 +0200

    Getting closer to complying with SUIT css naming.
    Signed-off-by: Simon Mellberg <mellbergsimon@example.com>

--- plugins/caspar/app/components/LibraryListFolder/index.jsx
+++ plugins/caspar/app/components/LibraryListFolder/index.jsx
@@ -3,50 +3,37 @@ import React, { useState } from "react"
 import { Icon } from '../Icon'
 import "./style.css"
 
-/**
- * Render a folder name with an arrow icon
- * 
- * @param {boolean} isOpen - Whether the folder is open or closed
- * @param {string} name - The name of the folder
- * @param {function} handleClick - Open or close the folder
- * @returns {JSX.Element} - Rendered folder name
- */
-const FolderName = ({ isOpen, name, handleClick }) => {
+const LibraryListFolderName = ({ isOpen, name, handleClick }) => {
   return (
-    <div className={`FolderName ${isOpen ? 'is-open' : ''}`} onClick={handleClick}>
-      <div className='FolderName-icon'>
+    <div
+      className={`LibraryListFolder-name ${isOpen ? 'is-open' : ''}`}
+      onClick={handleClick}
+    >
+      <div className='LibraryListFolder-nameIcon'>
         <Icon name='arrow-down' />
       </div>
-      <div className="FolderName-text">
+      <div className="LibraryListFolder-nameText">
         {name}
       </div>
     </div>    
   )
 }
 
-/**
- * Render a folder with a list of items
- * 
- * @param {string} name - The name of the folder
- * @param {Array} children - The list of items in the folder
- * @param {object} node - The node object containing the files
- * @returns {JSX.Element} - Rendered folder with items
- */
 const LibraryListFolder = ({ name, children, node }) => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <section className={'FolderWrapper'}>
-      <div className={'FolderHeader'}>
-        <FolderName
+    <section className={`LibraryListFolder ${isOpen ? 'is-open' : 'is-closed'}`}>
+      <div className='LibraryListFolder-header'>
+        <LibraryListFolderName
           name={name}
           isOpen={isOpen}
           handleClick={() => setIsOpen(!isOpen)}
         />
-        <div className="FolderItems">{node?.files?.length ?? 0} items</div>
+        <div className="LibraryListFolder-items">{node?.files?.length ?? 0} items</div>
       </div>
-      <div className={`CollapsibleFolder ${isOpen ? 'is-open' : 'is-closed'}`}>
-        <div className="VerticalLine">
+      <div className="LibraryListFolder-folder">
+        <div className="LibraryListFolder-line">
           {children}
         </div>
       </div>
