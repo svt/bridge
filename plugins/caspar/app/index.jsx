a74fea19703f05f53c95726e7815d4fe4ca66039
Axel Boberg <git@axelboberg.se>
2025-05-10 23:34:46 +0200

    Rollback React to v17 for the time being as performance was degraded with 18 and 19, probably due to the new concurrent rendering strategy
    Signed-off-by: Axel Boberg <git@axelboberg.se>

--- plugins/caspar/app/index.jsx
+++ plugins/caspar/app/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 
 import App from './App'
 
 import './style.css'
 
-const root = createRoot(document.getElementById('root'))
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 )
