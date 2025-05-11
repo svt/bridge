0f5d7ba32da4b0c52eb309631bcc9e7419b897c6
Simon Mellberg <mellbergsimon@example.com>
2025-05-11 22:35:41 +0200

    Reworked tests to have one test per case and combined some into into one test.
    Signed-off-by: Simon Mellberg <mellbergsimon@example.com>

--- plugins/caspar/app/utils/library.cjs
+++ plugins/caspar/app/utils/library.cjs
@@ -1,55 +1,101 @@
 const uuid = require('uuid')
 
 /**
- * Converts a list of file paths into a nested folder structure
- * The function assumes that paths are strings with folders and files separated by '/'
+ * Regex pattern to match file path delimiters
+ */
+const FILE_PATH_DELIMITER = /[\/\\]+/ // eslint-disable-line
+
+/**
+ * Regex pattern to match paths that end with a delimiter
+ */
+const ENDS_WITH_DELIMITER = /[\/\\]$/ // eslint-disable-line 
+
+/**
+ * Converts a list of file paths into a nested folder structure.
+ * The function assumes that paths are strings with folders and files separated by '/' or '\'.
  *
- * @param {Array} paths - An array of path objects, where each object contains at least a name and whole path
- * @returns {Array} - A nested folder structure represented as an array of folder objects
+ * @typedef {Object} File
+ * @property {boolean} file     - A flag indicating whether the object is a file (true) or a folder (false).
+ * @property {string} name      - The full path as a string (e.g., 'folder1/folder2/item').
+ * @property {string} id        - A unique identifier.
+ * @property {Object} [path]    - The original path object that contains additional properties such as caspar, data etc.
+ *
+ * @typedef {Object} Folder
+ * @property {boolean} file     - A flag indicating whether the object is a file (true) or a folder (false).
+ * @property {string} name      - The name of the file/folder.
+ * @property {string} id        - A unique identifier.
+ * @property {Array[]} [files]  - An array of nested files/folders.
+ * 
+ * @param {Array[]} paths - An array of path objects, where each object contains at least a `name` property with the full path.
+ * @returns {Folder[]} - A nested folder structure represented as an array of folder objects.
+ * @example 
+ * Input: [{ path.name: 'folder1/item' }]
+ * Output: [
+ *   {
+ *     file: false,
+ *     name: 'folder1',
+ *     id: 'uuid',
+ *     files: [
+ *       {
+ *          ...: existing properties,
+ *          file: true,
+ *          name: 'folder1/folder2/item',
+ *          id: 'uuid'       
+ *       }
+ *     ]
+ *   }
+ * ]
  */
 function buildFolderTree (paths) {
   if (!paths) return []
 
   const root = []
-  const delimiters = /[\/\\]+/ // eslint-disable-line 
 
   for (const path of paths) {
     // Split the path into parts by '/' or '\' and remove any empty segments
-    const parts = path.name.split(delimiters).filter(Boolean)
+    const parts = path.name.split(FILE_PATH_DELIMITER).filter(Boolean)
 
-    // If path ends with a delimiter, it's a folder path
-    const isFolderPath = /[\/\\]$/.test(path.name) // eslint-disable-line
+    const isFolderPath = ENDS_WITH_DELIMITER.test(path.name) // Check if the path ends with a delimiter
 
     let currentLevel = root
 
     parts.forEach((part, index) => {
-      const isLast = index === parts.length - 1
-      const isFile = !isFolderPath && isLast // Only the last part can be a file
+      const isFile = index === parts.length - 1 && !isFolderPath // Only the last part can be a file, unless the path ends with a delimiter
       const pwd = parts.slice(0, index + 1).join('/') // Join parts to get the path
 
       let existing = currentLevel.find((item) => item.name === part) // Check if folder exists on current level
 
-      if (!existing) { // If it does not exist then create it.
-        if (isFile) {
-          existing = {
-            ...path,
-            file: true,
-            name: pwd,
-            id: uuid.v4()
-          }
-        } else { // It is a folder then save name instead of path
-          existing = {
-            file: false,
-            name: part,
-            id: uuid.v4(),
-            files: []
-          }
+      // If the folder exists, go into it
+      if (existing) {
+        if (!isFile) {
+          currentLevel = existing.files
         }
-        currentLevel.push(existing) // Save folder or file to current level
+        return
+      }
+      
+      // Create new folder or file object
+      if (isFile) {
+        existing = {
+          ...path,
+          file: true,
+          name: pwd,
+          id: uuid.v4()
+        }
+      } else {
+        existing = {
+          file: false,
+          name: part,
+          id: uuid.v4(),
+          files: []
+        }
       }
 
+      // Add new object to current level
+      currentLevel.push(existing)
+      
+      // Descend into if it's a folder
       if (!isFile) {
-        currentLevel = existing.files // If the current item is a folder, go deeper
+        currentLevel = existing.files
       }
     })
   }
@@ -63,25 +109,14 @@ exports.buildFolderTree = buildFolderTree
  * @param {string} path The full file path.
  * @returns {string} The file name from the path.
  */
-function getFileName (path) {
-  if (!path) {
+function getFileName(path) {
+  // If path does not exist or path ends with a delimiter then return empty string.
+  if (!path || ENDS_WITH_DELIMITER.test(path)) {
     return ''
   }
 
-  // Normalize the path by removing leading/trailing slashes
-  path = path.replace(/^\/+|\/+$/g, '') // Unix-style paths
-  path = path.replace(/^\\+|\\+$/g, '') // Windows-style paths
-
-  // Handle edge case where the path is just a slash
-  if (!path) {
-    return ''
-  }
-
-  // Split the path using both slashes and backslashes
-  /* eslint-disable-next-line no-useless-escape */
-  const parts = path.split(/[\/\\]+/)
-
-  // Return the last part of the path, which should be the file name
+  //Split string and return the last part.
+  const parts = path.split(FILE_PATH_DELIMITER)
   return parts[parts.length - 1] || ''
 }
 exports.getFileName = getFileName
--- plugins/caspar/app/utils/library.unit.test.js
+++ plugins/caspar/app/utils/library.unit.test.js
@@ -1,176 +1,190 @@
 const { buildFolderTree, getFileName } = require('./library.cjs')
 
-describe('buildFolderTree function', () => {
-  const testCases = [
+test('multiple folders with same name', () => {
+  const input = [
+    { name: 'folder1/file1' },
+    { name: 'folder1/file2' }
+  ]
+
+  const expected = [
     {
-      description: 'Folder with one child',
-      input: [{ name: 'folder/file' }],
-      expected: [
+      file: false,
+      name: 'folder1',
+      id: expect.any(String),
+      files: [
         {
-          file: false,
-          name: 'folder',
-          id: expect.any(String),
-          files: [
-            {
-              file: true,
-              name: 'folder/file',
-              id: expect.any(String)
-            }
-          ]
-        }
-      ]
-    },
-    {
-      description: 'Undefined input should return empty array',
-      input: undefined,
-      expected: []
-    },
-    {
-      description: 'Empty input should return empty array',
-      input: [],
-      expected: []
-    },
-    {
-      description: 'Multiple delimiters should return correct structure',
-      input: [{ name: 'folder//file' }],
-      expected: [
+          file: true,
+          name: 'folder1/file1',
+          id: expect.any(String)
+        },
         {
-          file: false,
-          name: 'folder',
-          id: expect.any(String),
-          files: [
-            {
-              file: true,
-              name: 'folder/file',
-              id: expect.any(String)
-            }
-          ]
-        }
-      ]
-    },
-    {
-      description: 'Different delimiter (backslash) should return correct structure',
-      input: [{ name: 'folder\\file' }],
-      expected: [
-        {
-          file: false,
-          name: 'folder',
-          id: expect.any(String),
-          files: [
-            {
-              file: true,
-              name: 'folder/file',
-              id: expect.any(String)
-            }
-          ]
-        }
-      ]
-    },
-    {
-      description: 'File names with points and dashes should be handled correctly',
-      input: [{ name: 'folder/file.with-dash' }],
-      expected: [
-        {
-          file: false,
-          name: 'folder',
-          id: expect.any(String),
-          files: [
-            {
-              file: true,
-              name: 'folder/file.with-dash',
-              id: expect.any(String)
-            }
-          ]
-        }
-      ]
-    },
-    {
-      description: 'Trailing slash should be folder with no items',
-      input: [{ name: 'folder1/folder2/' }],
-      expected: [
-        {
-          file: false,
-          name: 'folder1',
-          id: expect.any(String),
-          files: [
-            {
-              file: false,
-              name: 'folder2',
-              id: expect.any(String),
-              files: []
-            }
-          ]
+          file: true,
+          name: 'folder1/file2',
+          id: expect.any(String)
         }
       ]
     }
   ]
 
-  testCases.forEach(({ description, input, expected }) => {
-    it(description, () => {
-      const result = buildFolderTree(input)
-      expect(result).toEqual(expected)
-    })
-  })
+  const result = buildFolderTree(input)
+  expect(result).toEqual(expected)
 })
 
-describe('getFileName function', () => {
-  const testCases = [
+test('Folder with one child', () => {
+  const input = [{ name: 'folder/file' }]
+  const expected = [
     {
-      description: 'Should return the correct file name for a simple path',
-      input: 'folder/file.txt',
-      expected: 'file.txt'
-    },
-    {
-      description: 'Should return the correct file name when path has multiple folders',
-      input: 'folder/subfolder/file.txt',
-      expected: 'file.txt'
-    },
-    {
-      description: 'Should handle paths with trailing slash',
-      input: 'folder/file/with/trailing/slash/',
-      expected: 'slash'
-    },
-    {
-      description: 'Should return the file name for a file in the root directory',
-      input: 'file.txt',
-      expected: 'file.txt'
-    },
-    {
-      description: 'Should return an empty string for an empty path',
-      input: '',
-      expected: ''
-    },
-    {
-      description: 'Should handle paths with multiple slashes correctly',
-      input: 'folder//file.txt',
-      expected: 'file.txt'
-    },
-    {
-      description: 'Should return the correct file name for path with backslashes',
-      input: 'folder\\file.txt',
-      expected: 'file.txt'
-    },
-    {
-      description: 'Should return the correct file name for a path with dots',
-      input: 'folder/file.with.dots.txt',
-      expected: 'file.with.dots.txt'
-    },
-    {
-      description: 'Should return the last part of path if file has no extension',
-      input: 'folder/file',
-      expected: 'file'
-    },
-    {
-      description: 'If path is undefined, should return empty string',
-      input: undefined,
-      expected: ''
+      file: false,
+      name: 'folder',
+      id: expect.any(String),
+      files: [
+        {
+          file: true,
+          name: 'folder/file',
+          id: expect.any(String)
+        }
+      ]
     }
   ]
 
-  testCases.forEach(({ description, input, expected }) => {
-    it(description, () => {
-      const result = getFileName(input)
-      expect(result).toEqual(expected)
-    })
-  })
+  const result = buildFolderTree(input)
+  expect(result).toEqual(expected)
+
+
+})
+
+test('Undefined or empty input should return empty array', () => {
+  let input = undefined
+  const expected = []
+  expect(buildFolderTree(input)).toEqual(expected)
+
+  input = []
+
+  expect(buildFolderTree(input)).toEqual(expected)
+})
+
+
+test('Multiple delimiters should return correct structure', () => {
+  const input = [{ name: 'folder///file' }]
+  const expected = [
+    {
+      file: false,
+      name: 'folder',
+      id: expect.any(String),
+      files: [
+        {
+          file: true,
+          name: 'folder/file',
+          id: expect.any(String)
+        }
+      ]
+    }
+  ]
+
+  const result = buildFolderTree(input)
+  expect(result).toEqual(expected)
+})
+
+test('Different delimiter (backslash) should return correct structure', () => {
+  const input = [{ name: 'folder\\file' }]
+  const expected = [
+    {
+      file: false,
+      name: 'folder',
+      id: expect.any(String),
+      files: [
+        {
+          file: true,
+          name: 'folder/file',
+          id: expect.any(String)
+        }
+      ]
+    }
+  ]
+
+  const result = buildFolderTree(input)
+  expect(result).toEqual(expected)
+})
+
+test('File names with points and dashes should be handled correctly', () => {
+  const input = [{ name: 'folder/file.with-dash' }]
+  const expected = [
+    {
+      file: false,
+      name: 'folder',
+      id: expect.any(String),
+      files: [
+        {
+          file: true,
+          name: 'folder/file.with-dash',
+          id: expect.any(String)
+        }
+      ]
+    }
+  ]
+
+  const result = buildFolderTree(input)
+  expect(result).toEqual(expected)
+})
+
+test('Trailing slash should be folder with no items', () => {
+  const input = [{ name: 'folder1/folder2/' }]
+  const expected = [
+    {
+      file: false,
+      name: 'folder1',
+      id: expect.any(String),
+      files: [
+        {
+          file: false,
+          name: 'folder2',
+          id: expect.any(String),
+          files: []
+        }
+      ]
+    }
+  ]
+
+  const result = buildFolderTree(input)
+  expect(result).toEqual(expected)
+})
+
+test('Empty or undefined path should return empty string', () => {
+  let input = undefined
+  const expected = ''
+  expect(getFileName(input)).toEqual(expected)
+
+  input = ''
+  expect(getFileName(input)).toEqual(expected)
+})
+
+test('Trailing slash should return empty string', () => {
+  const input = 'folder/file/'
+  const expected = ''
+  expect(getFileName(input)).toEqual(expected)
+})
+
+test('Delimiters should be handled correctly', () => {
+  let input = 'folder\\file'
+  const expected = 'file'
+
+  expect(getFileName(input)).toEqual(expected)
+
+  input = 'folder//file'
+  expect(getFileName(input)).toEqual(expected)
+
+  input = 'folder/file'
+  expect(getFileName(input)).toEqual(expected)
+})
+
+test('Multiple folders should return correct file name', () => {
+  const input = 'folder1/folder2/file'
+  const expected = 'file'
+  expect(getFileName(input)).toEqual(expected)
+})
+
+test('Dots and dashed should be handled correctly', () => {
+  const input = 'folder/file.with.dash'
+  const expected = 'file.with.dash'
+  expect(getFileName(input)).toEqual(expected)
 })
