908817200f4e9258cee71a3bba2bfa6546df3c6c
Simon Mellberg <mellbergsimon@example.com>
2025-05-12 22:43:05 +0200

    Fixed comments and changed some naming to make more sense.
    Signed-off-by: Simon Mellberg <mellbergsimon@example.com>

--- plugins/caspar/app/utils/library.cjs
+++ plugins/caspar/app/utils/library.cjs
@@ -3,26 +3,26 @@ const uuid = require('uuid')
 /**
  * Regex pattern to match file path delimiters
  */
-const FILE_PATH_DELIMITER = /[\/\\]+/ // eslint-disable-line
+const FILE_PATH_DELIMITER = /[\/\\]+/
 
 /**
  * Regex pattern to match paths that end with a delimiter
  */
-const ENDS_WITH_DELIMITER = /[\/\\]$/ // eslint-disable-line 
+const FOLDER_END_DELIMITER = /[\/\\]$/
 
 /**
  * @typedef {{
- *   file: boolean
- *   name: string
- *   id: string
- *   path?: Object // Original path object that may contain properties like caspar, data, etc.
+ *    file: true,   // Indicates this is a file (not a folder)
+ *    name: string, // The name of the file 
+ *    id: string,   // A unique identifier for the file
+ *    path?: Object // Optional additional properties (e.g., caspar, data, etc.)
  * }} File
- *
+ * 
  * @typedef {{
- *   file: boolean
- *   name: string
- *   id: string
- *   files?: Folder[] // Nested files or folders
+ *   file: false,             // Indicates this is a folder (not a file)
+ *   name: string,            // The name of the folder
+ *   id: string,              // A unique identifier for the folder
+ *   files: (Folder | File)[] // Array of nested files and/or folders
  * }} Folder
  */
 
@@ -30,41 +30,42 @@ const ENDS_WITH_DELIMITER = /[\/\\]$/ // eslint-disable-line
  * Converts a list of file paths into a nested folder structure.
  * The function assumes that paths are strings with folders and files separated by '/' or '\'.
  * 
- * @param {{ name: string, [key: string]: any }[]} paths - An array of path objects with at least a `name` string.
- * @returns {Folder[Folder|File]} - A nested folder structure represented as an array of folder objects.
+ * @param {{ name: string, [key: string]: any }[]} items - An array of path objects with at least a `name` string.
+ * @returns {Folder[]} - A nested folder structure represented as an array of folder objects.
  * @example 
  * Input: [{ path.name: 'folder1/item' }]
  * Output: [
  *   {
  *     file: false,
  *     name: 'folder1',
- *     id: 'uuid',
+ *     id: 'uuid1',
  *     files: [
  *       {
  *          ...: existing properties,
  *          file: true,
- *          name: 'folder1/folder2/item',
- *          id: 'uuid'       
+ *          name: 'folder1/item',
+ *          id: 'uuid2'       
  *       }
  *     ]
  *   }
  * ]
  */
-function buildFolderTree (paths) {
-  if (!paths) return []
+function buildFolderTree (items) {
+  if (!items) return []
 
   const root = []
 
-  for (const path of paths) {
+  for (const item of items) {
     // Split the path into parts by '/' or '\' and remove any empty segments
-    const parts = path.name.split(FILE_PATH_DELIMITER).filter(Boolean)
+    const parts = item.name.split(FILE_PATH_DELIMITER).filter(Boolean)
 
-    const isFolderPath = ENDS_WITH_DELIMITER.test(path.name) // Check if the path ends with a delimiter
+    const isFolderPath = FOLDER_END_DELIMITER.test(item.name) // Check if the path ends with a delimiter
 
     let currentLevel = root
 
     parts.forEach((part, index) => {
-      const isFile = index === parts.length - 1 && !isFolderPath // Only the last part can be a file, unless the path ends with a delimiter
+      const isLast = index === parts.length - 1
+      const isFile = isLast && !isFolderPath // Only the last part can be a file, unless the path ends with a delimiter
       const pwd = parts.slice(0, index + 1).join('/') // Join parts to get the path
 
       let existing = currentLevel.find((item) => item.name === part) // Check if folder exists on current level
@@ -80,7 +81,7 @@ function buildFolderTree (paths) {
       // Create new folder or file object
       if (isFile) {
         existing = {
-          ...path,
+          ...item,
           file: true,
           name: pwd,
           id: uuid.v4()
@@ -110,17 +111,17 @@ exports.buildFolderTree = buildFolderTree
 /**
  * Extracts the file name from a given path. Delimited by '/'.
  *
- * @param {string} path The full file path.
+ * @param {string} filePath The full file path.
  * @returns {string} The file name from the path.
  */
-function getFileName(path) {
+function getFileName(filePath) {
   // If path does not exist or path ends with a delimiter then return empty string.
-  if (!path || ENDS_WITH_DELIMITER.test(path)) {
+  if (!filePath || FOLDER_END_DELIMITER.test(filePath)) {
     return ''
   }
 
   //Split string and return the last part.
-  const parts = path.split(FILE_PATH_DELIMITER)
+  const parts = filePath.split(FILE_PATH_DELIMITER)
   return parts[parts.length - 1] || ''
 }
 exports.getFileName = getFileName
