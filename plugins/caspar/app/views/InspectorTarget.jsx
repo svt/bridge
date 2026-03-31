ea0e118053c88bb5d262d7cb3ef0113200932108
Axel Boberg <axel.boberg@svt.se>
2026-03-31 11:11:23 +0200

    Remove unused logic from the InspectorTarget component
    Signed-off-by: Axel Boberg <axel.boberg@svt.se>

--- plugins/caspar/app/views/InspectorTarget.jsx
+++ plugins/caspar/app/views/InspectorTarget.jsx
@@ -1,40 +1,7 @@
 import React from 'react'
 import bridge from 'bridge'
 
-import { SharedContext } from '../sharedContext'
-
-function objToQueryString (obj = {}) {
-  if (typeof obj !== 'object') {
-    return
-  }
-
-  return Object.entries(obj)
-    .map(([key, val]) => {
-      return `${encodeURIComponent(key)}=${encodeURIComponent(val)}`
-    })
-    .join('&')
-}
-
 export const InspectorTarget = () => {
-  const [state] = React.useContext(SharedContext)
-  const [selection, setSelection] = React.useState([])
-  const selectionRef = React.useRef([])
-
-  React.useEffect(() => {
-    async function updateSelection () {
-      const selection = await bridge.client.selection.getSelection()
-      selectionRef.current = selection
-      setSelection(selection)
-    }
-    updateSelection()
-  }, [state])
-
-  function handleNewValue (set) {
-    for (const id of selectionRef.current) {
-      bridge.items.applyItem(id, set, true)
-    }
-  }
-
   async function handleButtonClick () {
     const modalId = bridge.ui.modal.makeId()
     bridge.ui.modal.open({
