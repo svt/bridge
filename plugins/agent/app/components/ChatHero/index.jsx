df67bf76c72d612534c7a0081da615670fe8c500
Axel Boberg <git@axelboberg.se>
2026-04-17 20:38:20 +0200

    Tweak the prompt for proof reading
    Signed-off-by: Axel Boberg <git@axelboberg.se>

--- plugins/agent/app/components/ChatHero/index.jsx
+++ plugins/agent/app/components/ChatHero/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './style.css'
 
 const PROMPTS = {
-  PROOF_READ: 'Help me proof read all items in the rundown and please let me know if I\'ve made any mistakes.'
+  PROOF_READ: 'Help me proof read all items in the rundown and please let me know if I\'ve made any mistakes. Prioritize template data and naming. Return an overview in list form of the strings that might be misspelled. Do not list unnecessary information.'
 }
 
 export function ChatHero ({ onSend = () => {} }) {
