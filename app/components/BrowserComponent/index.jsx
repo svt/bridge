73e4c22f16202521803c402f9658a639f1a5fe86
Axel Boberg <git@axelboberg.se>
2026-07-20 00:37:25 +0200

    Tweak browser header
    Signed-off-by: Axel Boberg <git@axelboberg.se>

--- app/components/BrowserComponent/index.jsx
+++ app/components/BrowserComponent/index.jsx
@@ -4,15 +4,22 @@ import './style.css'
 import { BrowserComponentHeader } from './BrowserComponentHeader'
 
 export const BrowserComponent = ({ data = {}, onUpdate = () => {} }) => {
+  const [isLoading, setIsLoading] = React.useState(Boolean(data?.uri))
+
+  React.useEffect(() => {
+    setIsLoading(Boolean(data?.uri))
+  }, [data?.uri])
+
   return (
     <div className='BrowserComponent'>
       <div className='BrowserComponent-header'>
         <BrowserComponentHeader uri={data?.uri} onChange={newUri => onUpdate({ uri: newUri })} />
       </div>
+      {isLoading && <div className='BrowserComponent-loadingBar' />}
       <div className='BrowserComponent-frameContainer'>
         {
           data?.uri &&
-          <iframe className='BrowserComponent-frame' src={data?.uri} />
+          <iframe className='BrowserComponent-frame' src={data?.uri} onLoad={() => setIsLoading(false)} onError={() => setIsLoading(false)} />
         }
       </div>
     </div>
