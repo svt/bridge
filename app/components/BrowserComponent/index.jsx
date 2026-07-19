7562e211526ad009464e3e6aa3d461daf4cb7377
Axel Boberg <git@axelboberg.se>
2026-07-20 00:25:41 +0200

    Implement website loading
    Signed-off-by: Axel Boberg <git@axelboberg.se>

--- app/components/BrowserComponent/index.jsx
+++ app/components/BrowserComponent/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './style.css'
 
-export const BrowserComponent = ({ data = {} }) => {
+import { BrowserComponentHeader } from './BrowserComponentHeader'
+
+export const BrowserComponent = ({ data = {}, onUpdate = () => {} }) => {
   return (
     <div className='BrowserComponent'>
-      <div className='BrowserComponent-container'>
-        <div className='BrowserComponent-heading'>Browser</div>
-        {data.component}
+      <div className='BrowserComponent-header'>
+        <BrowserComponentHeader uri={data?.uri} onChange={newUri => onUpdate({ uri: newUri })} />
+      </div>
+      <div className='BrowserComponent-frameContainer'>
+        {
+          data?.uri &&
+          <iframe className='BrowserComponent-frame' src={data?.uri} />
+        }
       </div>
     </div>
   )
